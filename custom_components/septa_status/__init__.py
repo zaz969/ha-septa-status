955f7022521d22c2d0b0e9bcc35a18a6276789ed
Matt M. <zaz969@protonmail.com>
2025-12-01 20:34:54 -0500

    Fix missing async_setup_entry

--- custom_components/septa_status/__init__.py
+++ custom_components/septa_status/__init__.py
@@ -1,19 +1,54 @@
+from dataclasses import dataclass
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import ConfigEntryNotReady
-from homeassistant.const import CONF_ADDRESS
 
 from .const import DOMAIN
+from .coordinator import SeptaStatusCoordinator
 
 PLATFORMS: list[Platform] = [Platform.SENSOR]
+
+@dataclass
+class RuntimeData:
+    """Class to hold your data."""
+
+    coordinator: DataUpdateCoordinator
+    cancel_update_listener: Callable
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     """Set up Integration from a config entry."""
 
     hass.data.setdefault(DOMAIN, {})
 
+    # Initialise the coordinator that manages data updates from your api.
+    # This is defined in coordinator.py
+    coordinator = SeptaStatusCoordinator(hass, config_entry)
 
+    # Perform an initial data load from api.
+    # async_config_entry_first_refresh() is special in that it does not log errors if it fails
+    await coordinator.async_config_entry_first_refresh()
+
+    # Initialise a listener for config flow options changes.
+    # See config_flow for defining an options setting that shows up as configure on the integration.
+    cancel_update_listener = config_entry.add_update_listener(_async_update_listener)
+
+    # Add the coordinator and update listener to hass data to make
+    hass.data[DOMAIN][config_entry.entry_id] = RuntimeData(
+        coordinator, cancel_update_listener
+    )
+
+    # Setup platforms (based on the list of entity types in PLATFORMS defined above)
+    # This calls the async_setup method in each of your entity type files.
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
+
+    # Return true to denote a successful setup.
+    return True
+
+async def _async_update_listener(hass: HomeAssistant, config_entry):
+    """Handle config options update."""
+    # Reload the integration when the options change.
+    await hass.config_entries.async_reload(config_entry.entry_id)
 
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
