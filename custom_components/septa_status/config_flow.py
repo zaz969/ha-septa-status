474ad1362caa43a4759cd0cd318f8fd4487dff79
Matt M. <zaz969@protonmail.com>
2025-12-01 20:53:51 -0500

    Fix config_flow.py issues with steps not being named right.

--- custom_components/septa_status/config_flow.py
+++ custom_components/septa_status/config_flow.py
@@ -5,26 +5,19 @@ import voluptuous as vol
 from homeassistant.config_entries import ConfigFlow
 from homeassistant.data_entry_flow import FlowResult
 
-
 from .const import DOMAIN
+
 
 class SeptaConfigFlow(ConfigFlow, domain=DOMAIN):
     VERSION = 1
-    def __init__(self):
+
+    def __init__(self) -> None:
         self._errors = {}
 
+    async def async_step_user(self) -> FlowResult:
+        await self.async_set_unique_id("septa_status_listener")
+        self._abort_if_unique_id_configured()
 
-    async def async_step_init(self) -> FlowResult:
-
-        return self.async_show_form(step_id='init', data_schema=vol.Schema({
+        return self.async_show_form(step_id='user', data_schema=vol.Schema({
             vol.Required("interval", default=30): int
         }))
-
-    async def async_step_finalize(self, user_input: dict[str, Any] | None = None):
-        self._abort_if_unique_id_configured()
-        await self.async_set_unique_id()
-
-        if user_input is not None:
-            return self.async_create_entry(title=DOMAIN, data={
-                "interval": user_input["interval"]
-            })
